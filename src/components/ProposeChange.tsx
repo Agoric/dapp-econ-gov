85912c14f3880129908b026eab29fbc3835a2e10
Turadg Aleahmad <turadg@agoric.com>
2022-09-26 14:17:27 -0700

    loading governance params with jotai

--- src/components/ProposeChange.tsx
+++ src/components/ProposeChange.tsx
@@ -1,8 +1,7 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 /* eslint-disable jsx-a11y/anchor-has-content */
-import clsx from 'clsx';
-import { WalletContext } from 'lib/wallet';
-import { useContext } from 'react';
+import { useAtomValue } from 'jotai';
+import { displayFunctionsAtom } from 'store/app';
 import { ParameterValue } from './PsmGovernance';
 
 interface Props {
@@ -11,10 +10,28 @@ interface Props {
 }
 
 export default function ProposeChange(props: Props) {
+  const { displayAmount } = useAtomValue(displayFunctionsAtom);
+  const { currentValue, name } = props;
+  let changer = <i>{currentValue.type} not supported</i>;
+  switch (currentValue.type) {
+    case 'amount':
+      changer = (
+        <div>
+          <p>Current value: {displayAmount(currentValue.value, 2)}</p>
+          <p>
+            Proposed new value: <input type="number" />
+          </p>
+        </div>
+      );
+      break;
+  }
   return (
     <fieldset>
-      <legend>PROPOSE CHANGE: {props.name}</legend>
-      Current value: ??? Proposed new value: <input type="number" />
+      <legend>
+        <h3>{name}</h3>
+      </legend>
+      {changer}
+      <br />
       <button>Submit proposal</button>
     </fieldset>
   );
--- src/components/PsmGovernance.tsx
+++ src/components/PsmGovernance.tsx
@@ -1,10 +1,10 @@
 import { Amount } from '@agoric/ertp';
-import { WalletContext } from 'lib/wallet';
-import { useContext, useEffect, useReducer, useState } from 'react';
+import { useAtomValue } from 'jotai';
+import { governedParamsIndexAtom } from 'store/app';
 import ProposeChange from './ProposeChange';
 
 interface Props {
-  instanceName: string;
+  anchorName: string;
 }
 
 export type ParameterValue =
@@ -21,46 +21,19 @@ export type ParameterValue =
       value: Amount;
     };
 
-const useRpcDatum = path => {
-  const [status, setStatus] = useState('idle');
-  const [data, setData] = useState([]);
-  const walletUtils = useContext(WalletContext);
-
-  useEffect(() => {
-    const { follow } = walletUtils;
-    // FIXME match instanceName, pass as tuple instead
-    follow(`:published.psm.IST.AUSD.governance`).then(async follower => {
-      setStatus('following');
-      const iterable: AsyncIterable<Record<string, unknown>> =
-        await follower.getLatestIterable();
-      setStatus('got iterable');
-      const iterator = iterable[Symbol.asyncIterator]();
-      setStatus('got iterator');
-      const { value: publishedValue } = await iterator.next();
-      setStatus('got value');
-      setData(publishedValue);
-      console.log('called setData', publishedValue);
-      setStatus('received');
-    });
-  }, [path, walletUtils]);
-
-  return { status, data };
-};
-
 export default function PsmGovernance(props: Props) {
-  const { status, data } = useRpcDatum(':published.psm.IST.AUSD.governance');
+  const governedParamsIndex = useAtomValue(governedParamsIndexAtom);
 
-  console.log('render PsmGovernance', status, data);
-  const params = null;
+  const params = governedParamsIndex.get(props.anchorName);
 
   return (
     <div>
-      <h2>{props.instanceName}</h2>
+      <h2>{props.anchorName}</h2>
       {params
         ? Object.entries(params).map(([name, value]) => (
             <ProposeChange key={name} name={name} currentValue={value} />
           ))
-        : 'loading…'}
+        : 'signal wallet!'}
     </div>
   );
 }
