b277df930357f4287042dcf85e22ab7b19056080
Fraz Arshad <fraz.arshad12@gmail.com>
2025-03-04 12:34:47 +0500

    test: updated wallet phrases for updated committee

--- tests/e2e/utils.js
+++ tests/e2e/utils.js
@@ -4,14 +4,19 @@ export const phrasesList = {
   emerynet: {
     isLocal: false,
     minutes: 3,
-    token: 'ToyUSD',
+    token: 'USDT_axl',
     network: 'emerynet',
     networkConfigURL: 'https://emerynet.agoric.net/network-config',
-    gov1Phrase: Cypress.env('GOV1_PHRASE'),
-    gov2Phrase: Cypress.env('GOV2_PHRASE'),
+    gov1Phrase:
+      'such field health riot cost kitten silly tube flash wrap festival portion imitate this make question host bitter puppy wait area glide soldier knee',
+    gov2Phrase:
+      'physical immune cargo feel crawl style fox require inhale law local glory cheese bring swear royal spy buyer diesel field when task spin alley',
     // UNTIL https://github.com/Agoric/dapp-econ-gov/issues/144
-    gov3Phrase: Cypress.env('GOV4_PHRASE'),
-    gov4Phrase: Cypress.env('GOV3_PHRASE'),
+    gov3Phrase:
+      'spike siege world rather ordinary upper napkin voice brush oppose junior route trim crush expire angry seminar anchor panther piano image pepper chest alone',
+    gov4Phrase:
+      'smile unveil sketch gaze length bulb goddess street case exact table fetch robust chronic power choice endorse toward pledge dish access sad illegal dance',
+ 
   },
   devnet: {
     isLocal: false,
